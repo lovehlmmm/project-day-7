64e727ab56c9c29df789c22961a801b41ee82490
lovehlmmm <mboxshop92@gmail.com>
2018-09-06 03:49:24 +0700

    add material & order dashbroad

---
+++ ProjectGroup6/WEB/Areas/Admin/Controllers/MaterialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB.Areas.Admin.Controllers
+{
+    public class MaterialController : Controller
+    {
+        // GET: Admin/Material
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
