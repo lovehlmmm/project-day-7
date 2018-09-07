8fac8225e6665e53d4e4d32de4b7ed19187c7028
lovehlmmm <mboxshop92@gmail.com>
2018-09-08 04:41:27 +0700

    update user management

--- Project/WEB/Areas/Admin/Controllers/UserController.cs
+++ Project/WEB/Areas/Admin/Controllers/UserController.cs
@@ -13,5 +13,10 @@ namespace WEB.Areas.Admin.Controllers
         {
             return View();
         }
+        public ActionResult Details()
+        {
+            return View();
+        }
+
     }
 }
