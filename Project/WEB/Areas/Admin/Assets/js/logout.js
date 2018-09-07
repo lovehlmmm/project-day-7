a21cf8fdb00ea54d361f94175fbdf75309a918e8
lovehlmmm <mboxshop92@gmail.com>
2018-09-07 19:41:31 +0700

    update logout

---
+++ Project/WEB/Areas/Admin/Assets/js/logout.js
@@ -0,0 +1,19 @@
+﻿$('#btnLogout').click(function () {
+    $.ajax({
+        url: '/admin/Login/Logout',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            if (data.status === true) {
+                window.location = data.url;
+            } else {
+                swal({
+                    type: 'error',
+                    title: 'Logout Fail',
+                    text: 'Logout Fail! Please try again ♥'
+                });
+            }
+        }
+    });
+
+});
--- Project/WEB/Areas/Admin/Controllers/LoginController.cs
+++ Project/WEB/Areas/Admin/Controllers/LoginController.cs
@@ -33,20 +33,38 @@ namespace WEB.Areas.Admin.Controllers
             try
             {
                 LoginUser userLogin = JsonConvert.DeserializeObject<LoginUser>(user);
-                var checkUser= _userService.CheckLogin(userLogin.Username.Trim(), userLogin.Password.Trim());
-                if (checkUser!= null)
+                var checkUser = _userService.CheckLogin(userLogin.Username.Trim(), userLogin.Password.Trim());
+                if (checkUser != null)
                 {
-                    if(!checkUser.Role.Equals(UserRole.Admin)) return Json(new { status = false });
+                    if (!checkUser.Role.Equals(UserRole.Admin)) return Json(new { status = false });
                     UserSession userSession = new UserSession(checkUser.Username, checkUser.Role);
                     SessionHelper.SetSession(userSession, AppSettingConstant.LoginSessionAdmin);
-                    return Json(new { status = true,url = "/admin/home"}, JsonRequestBehavior.AllowGet);
+                    return Json(new { status = true, url = "/admin/home" }, JsonRequestBehavior.AllowGet);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-            return Json(new {status=false});
+            return Json(new { status = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult Logout()
+        {
+            try
+            {
+                var checkSession = SessionHelper.GetSession(AppSettingConstant.LoginSessionAdmin);
+                if (checkSession != null)
+                {
+                    SessionHelper.Delete(AppSettingConstant.LoginSessionAdmin);
+                    return Json(new { status = true, url="/admin" },JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return Json(new { status = false }, JsonRequestBehavior.AllowGet);
         }
     }
 }
