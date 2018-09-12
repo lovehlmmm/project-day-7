4f62df5b2619d46f74ce3d9706111de5b71f0037
lovehlmmm <mboxshop92@gmail.com>
2018-09-12 22:02:28 +0700

    update controller users

---
+++ Project/WEB/Controllers/OrderDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB.Controllers
+{
+    public class OrderDetailsController : Controller
+    {
+        // GET: OrderDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project/WEB/Controllers/UserOrdersHistoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB.Controllers
+{
+    public class UserOrdersHistoryController : Controller
+    {
+        // GET: UserOrdersHistory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
