91e0908d780f0e92f12baf8e1a3650a0de20a20b
lovehlmmm <mboxshop92@gmail.com>
2018-09-13 08:36:29 +0700

    udpate validate

--- Project/WEB/Scripts/login.js
+++ Project/WEB/Scripts/login.js
@@ -4,18 +4,18 @@ $("#formLogin").submit(function () {
     var username = $("input[name=username]").val().trim();
     var password = $("input[name=password]").val().trim();
     var remember = $("input[name=remember]:checked").length > 0;
-    if (username === '') {
-        $("input[name=username]").addClass("border border-danger");
-        return false;
-    } else {
-        $("input[name=username]").removeClass("border border-danger");
-    }
-    if (password === '') {
-        $("input[name=password]").addClass("border border-danger");
-        return false;
-    } else {
-        $("input[name=password]").removeClass("border border-danger");
-    }
+    //if (username === '') {
+    //    $("input[name=username]").addClass("border border-danger");
+    //    return false;
+    //} else {
+    //    $("input[name=username]").removeClass("border border-danger");
+    //}
+    //if (password === '') {
+    //    $("input[name=password]").addClass("border border-danger");
+    //    return false;
+    //} else {
+    //    $("input[name=password]").removeClass("border border-danger");
+    //}
     var user = { Username: username, Password: password, Remember: remember };
     $.ajax({
         url: '/LoginUser/CheckLogin',
@@ -37,3 +37,47 @@ $("#formLogin").submit(function () {
     });
     return false;
 }); 
+
+$(document).ready(function () {
+    $("#formLogin").validate({
+        rules: {
+ 
+            username: {
+                required: true,
+                minlength: 2
+            },
+            password: {
+                required: true,
+                minlength: 5
+            },
+ 
+            agree: "required"
+        },
+        messages: {
+            
+            username: {
+                required: "Please enter a username",
+                minlength: "Your username must consist of at least 2 characters"
+            },
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+        },
+        highlight: function (element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function (element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function (error, element) {
+            if (element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        },
+    });
+ });
--- Project/WEB/Scripts/register.js
+++ Project/WEB/Scripts/register.js
@@ -1,34 +1,101 @@
 ﻿$("#form-register").submit(function () {
-    
+
     var username = $("input[name=username]").val().trim();
     var password = $("input[name=password]").val().trim();
     var email = $("input[name=email]").val().trim();
     var name = $("input[name=name]").val().trim();
-    var gender=$('input[name=gender]:checked', '#form-register').val();
-    var remember = $("input[name=remember]:checked").length > 0;
-    if (username === '' || password === '') {
+    var gender = $('input[name=gender]:checked', '#form-register').val();
+     if (username === '' || password === '') {
         alert('fail');
         return false;
     }
-    var user = { Username: username, Password: password, Email: email};
+    var user = { Username: username, Password: password, Email: email };
     var cus = { CustomerName: name, Gender: gender };
     $.ajax({
         url: '/RegisterUser/Register',
-        type: 'POST',   
+        type: 'POST',
         dataType: 'json',
-        data: {user:user,customer:cus},
+        data: { user: user, customer: cus },
         async: true,
         success: function (data) {
             if (data.status === true) {
-                swal("Success", "Login Success", "success");
+                swal("Success", "Register Success", "success");
             } else {
                 swal({
                     type: 'error',
-                    title: 'Login Fail',
-                    text: 'Username password incorrect!'
+                    title: 'Register Fail',
+                    text: 'Something Wrong !'
                 });
             }
         }
     });
     return false;
-}); 
+});
+
+$.validator.setDefaults({
+    submitHandler: function () {
+        alert("submitted!");
+    }
+});
+
+$(document).ready(function () {
+    $("#form-register").validate({
+        rules: {
+            name: "required",
+            username: {
+                required: true,
+                minlength: 2
+            },
+            password: {
+                required: true,
+                minlength: 5
+            },
+            confirm_password: {
+                required: true,
+                minlength: 5,
+                equalTo: "#password"
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            gender: "required"
+        },
+        messages: {
+            name: "Please enter your name",
+            username: {
+                required: "Please enter a username",
+                minlength: "Your username must consist of at least 2 characters"
+            },
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+            confirm_password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long",
+                equalTo: "Please enter the same password as above"
+            },
+            email: "Please enter a valid email address",
+            gender: "Please choose you gender !"
+        },
+        highlight: function (element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function (element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function (error, element) {
+            if (element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+ 
+        } else if(element.prop('type') === 'radio') {
+                error.appendTo(element.parent().parent().parent());
+            } else {
+                error.insertAfter(element);
+            }
+        },
+    });
+});
