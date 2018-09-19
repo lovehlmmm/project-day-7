926af383642e3fc08e7821ddca39821d30fbade0
lovehlmmm <mboxshop92@gmail.com>
2018-09-19 20:14:46 +0700

    update

--- Project/WEB/Controllers/UserInfoController.cs
+++ Project/WEB/Controllers/UserInfoController.cs
@@ -1,16 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Constants;
+using Entities;
+using Helpers;
+using Services;
+using System;
 using System.Web.Mvc;
 
 namespace WEB.Controllers
 {
     public class UserInfoController : Controller
     {
+
+        private readonly IUserService _userService;
+
+        public UserInfoController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         // GET: UserInfo
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpGet]
+
+        public ActionResult GetUser()
+        {
+            var userSession = SessionHelper.GetSession(AppSettingConstant.LoginSessionCustomer) as User;
+            if (userSession != null)
+            {
+                var user = _userService.Find(u => u.Username == userSession.Username);
+                if (user != null)
+                {
+                    user.Password = "";
+                    ViewBag.ShowUser = user;
+                    return View();
+                }
+
+            }
+            return Redirect("/home");
+        }
+
+        public ActionResult UpdateUser(Customer customer )
+        {
+ 
             return View();
         }
     }
