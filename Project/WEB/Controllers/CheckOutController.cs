23fa8af569ddf2bbbe3b77c85bd8e1d491e77d75
lovehlmmm <mboxshop92@gmail.com>
2018-09-22 21:22:45 +0700

    update checkout

--- Project/WEB/Controllers/CheckOutController.cs
+++ Project/WEB/Controllers/CheckOutController.cs
@@ -1,17 +1,62 @@
-﻿using System;
+﻿using Constants;
+using Entities;
+using Helpers;
+using Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WEB.Models;
 
 namespace WEB.Controllers
 {
     public class CheckOutController : Controller
     {
+
+        private readonly IBaseService<Product> _productRepository;
+        private readonly IBaseService<Material> _materialRepository;
+        private readonly IBaseService<Address> _addressRepository;
+        private readonly IBaseService<User> _userRepository;
+
+
+        public CheckOutController(IBaseService<Product> productRepository, IBaseService<Material> materialRepository , IBaseService<Address> addressRepository, IBaseService<User> userRepository)
+        {
+            _productRepository = productRepository;
+            _materialRepository = materialRepository;
+            _addressRepository = addressRepository;
+            _userRepository = userRepository;
+        }
         // GET: CheckOut
         public ActionResult Index()
         {
-            return View();
+            UserSession userSession = SessionHelper.GetSession(AppSettingConstant.LoginSessionCustomer) as UserSession;
+           
+            if (userSession!=null)
+            {
+                var user = _userRepository.Find(u => u.Status.Equals(Status.Active) && u.Username.Equals(userSession.Username));
+                if (user!=null)
+                {
+                    var cart = SessionHelper.GetSession(AppSettingConstant.CartSession) as List<CartItem>;
+                    ViewBag.Cart = cart;
+                    ViewBag.Address = user.Customer.Addresses;
+                    return View();
+                }
+               
+            }
+                 return Redirect("/home");
         }
+
+        public ActionResult GetModalAddress()
+        {
+             return PartialView("~/Views/CheckOut/ModalAddress.cshtml");
+        }
+        public ActionResult GetModalAddAddress()
+        {
+            return PartialView("~/Views/CheckOut/ModalAddAddressPartial.cshtml");
+
+        }
+
+
     }
 }
---
+++ Project/WEB/Scripts/checkOut.js
@@ -0,0 +1,33 @@
+﻿ $('#showAddress').click(function () {
+      GetModalAddress();
+    });
+ 
+
+function GetModalAddress() {
+    $.ajax({
+        url: '/CheckOut/GetModalAddress',
+        contentType: 'application/html;charset=utf-8',
+        type: 'get',
+        async: false,
+        dataType: 'html'
+    }).success(function (result) {
+        $('#modalAddress').html(result);
+$('#modalAddress').modal();
+    }).error(function (xhr, status) {
+    });
+}
+
+function GetModalAddAddress() {
+    $.ajax({
+        url: '/CheckOut/GetModalAddAddress',
+        contentType: 'application/html;charset=utf-8',
+        type: 'get',
+        async: false,
+        dataType: 'html'
+    }).success(function (result) {
+        $('#addAddress').remove();
+        $('#modalAddress h5').text("Add a new receiving address")
+        $('#modalAddress .modal-body').html(result);
+    }).error(function (xhr, status) {
+    });
+}
