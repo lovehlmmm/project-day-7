ae115d310efe49359cbe9e5cead3def73061945e
neihc1002 <duongtran845@gmail.com>
2018-09-22 12:35:31 +0700

    update

---
+++ Project/Entities/Material.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+    public partial class Material : ModelExtension
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("material_id")]
+        public int Id { get; set; }
+        [StringLength(50)]
+        [Column("material_name")]
+        public string Name { get; set; }
+        [StringLength(50)]
+        [Column("material_details")]
+        public string Details { get; set; }
+        [Column("material_price", TypeName = "money")]
+        public decimal? Price { get; set; }
+    }
+}
