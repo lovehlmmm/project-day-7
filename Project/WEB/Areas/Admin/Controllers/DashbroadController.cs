da6d79c44f02beb04ef00c40b402fb2f33a67294
neihc1002 <duongtran845@gmail.com>
2018-09-28 22:06:08 +0700

    update

--- Project/WEB/Areas/Admin/Controllers/DashbroadController.cs
+++ Project/WEB/Areas/Admin/Controllers/DashbroadController.cs
@@ -26,17 +26,51 @@ namespace WEB.Areas.Admin.Controllers
             var previousYear = DateTime.Now.AddMonths(-1).Year;
             var month = DateTime.Now.Month;
             var year = DateTime.Now.Year;
-            var orderPrevious = _orderService.FindAll(o=>o.ModifiedAt.Value.Month.Equals(previousMonth) & o.ModifiedAt.Value.Year.Equals(previousYear)).ToList();
-            var orderMonth = _orderService.FindAll(o => o.ModifiedAt.Value.Month.Equals(month) & o.ModifiedAt.Value.Year.Equals(year) & o.Status == OrderStatus.Received).ToList();
+            var today = DateTime.Now.Date;
+            var privious = DateTime.Now.AddDays(-1);
+            var orderPrevious = _orderService.FindAll(o=>o.CreatedAt.Value.Month.Equals(previousMonth) & o.CreatedAt.Value.Year.Equals(previousYear)).ToList();
+            var orderMonth = _orderService.FindAll(o => o.CreatedAt.Value.Month.Equals(month) & o.CreatedAt.Value.Year.Equals(year) & o.Status == OrderStatus.Received).ToList();
             var orderPending = _orderService.FindAll(o => o.Status == OrderStatus.Pending).ToList();
+            int imageOfDay = orderMonth.Where(o => o.CreatedAt.Value.Date == today).Sum(od => od.OrderDetails.Sum(o => o.Quantity.Value));
+            int imageOfMonth = orderMonth.Sum(od => od.OrderDetails.Sum(o => o.Quantity.Value));
+            int imageOfDayPrevious = orderMonth.Where(o => o.CreatedAt.Value.Date == privious).Sum(od => od.OrderDetails.Sum(o => o.Quantity.Value));
+            int imageOfMonthPrevious = orderPrevious.Sum(od => od.OrderDetails.Sum(o => o.Quantity.Value));
+
+            float percentImageOfDay = 0;
+            if (imageOfDayPrevious>0)
+            {
+                percentImageOfDay = (imageOfDay - imageOfDayPrevious) / imageOfDayPrevious * 100;
+            }
+            else
+            {
+                percentImageOfDay = (imageOfDay - imageOfDayPrevious)* 100;
+            }
+            //Percent Day
+            float percentImageOfMonth = 0;
+            if (imageOfMonthPrevious > 0)
+            {
+                percentImageOfMonth = (imageOfMonth - imageOfMonthPrevious) / imageOfMonthPrevious * 100;
+            }
+            else
+            {
+               percentImageOfMonth=  (imageOfMonth - imageOfMonthPrevious) * 100;
+            }
+
+
             ViewModelDashboard viewModel = new ViewModelDashboard()
             {
                 OrderCount = orderMonth.Count,
-                OrderPending = orderPending.OrderByDescending(o=>o.CreatedAt).ToList(),
+                OrderPending = orderPending.OrderByDescending(o => o.CreatedAt).ToList(),
                 PreviousOrder = orderPrevious.Count,
                 Total = orderMonth.Sum(o => o.OrderDetails.Sum(od => od.Quantity * (od.Product.ProductPrice + od.Material.Price.Value))).Value,
-                TotalPrevious  = orderPrevious.Sum(o => o.OrderDetails.Sum(od => od.Quantity * (od.Product.ProductPrice + od.Material.Price.Value))).Value
+                TotalPrevious = orderPrevious.Sum(o => o.OrderDetails.Sum(od => od.Quantity * (od.Product.ProductPrice + od.Material.Price.Value))).Value,
+                ImageOfDay = imageOfDay,
+                ImageOfMonth = imageOfMonth,
+                PercentImageOfDay = percentImageOfDay,
+                PercentImageOfMonth = percentImageOfMonth
+
             };
+
             return View(viewModel);
         }
     }
--- Project/WEB/Areas/Admin/Models/ViewModelDashboard.cs
+++ Project/WEB/Areas/Admin/Models/ViewModelDashboard.cs
@@ -13,5 +13,9 @@ namespace WEB.Areas.Admin.Models
         public int PreviousOrder { get; set; }
         public decimal TotalPrevious { get; set; }
         public decimal Total { get; set; }
+        public int ImageOfDay  { get; set; }
+        public int ImageOfMonth { get; set; }
+        public float PercentImageOfDay { get; set; }
+        public float PercentImageOfMonth { get; set; }
     }
 }
