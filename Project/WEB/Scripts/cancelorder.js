63cf9e612648f36ec70e859d85e14640db1255bf
Tran Duong <duongtran845@gmail.com>
2018-09-28 11:02:22 +0700

    add cancelorder.js

---
+++ Project/WEB/Scripts/cancelorder.js
@@ -0,0 +1,39 @@
+﻿$('#btnCancel').click(function (){
+    var id = $(this).data('id');
+    swal({
+        title: "Are you sure?",
+        text: "Make sure ! When you cancel your order, you will receive a refund..!",
+        icon: "success",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willCancel) => {
+            if (willCancel) {
+                $('#loading').show();
+                ConfirmCancel(id);
+             }
+        });
+})
+
+function ConfirmCancel(id) {
+    $('#loading').show();
+    $.ajax({
+        url: '/OrderDetails/CancelOrder?id=' + id,
+        type: 'GET',
+         async: false,
+        processData: false,
+        contentType: false
+    }).success(function (result) {
+        if (result.status) {
+            $('#loading').hide();
+            swal("OK! Cancel success  !", {
+                icon: "success"
+            });
+            window.location.replace(result.url)
+        } else {
+            $('#loading').hide();
+            swal("Error", result.message, "error");
+        }
+    }).error(function (xhr, status) {
+    });
+}
