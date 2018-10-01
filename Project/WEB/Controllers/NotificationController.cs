0dd4b047b947540ede020c25ecd80e632e83a49f
neihc1002 <duongtran845@gmail.com>
2018-10-01 23:24:00 +0700

    update

---
+++ Project/WEB/Controllers/NotificationController.cs
@@ -0,0 +1,41 @@
+﻿using Constants;
+using Entities;
+using Helpers;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IBaseService<Notification> _notificationService;
+        public NotificationController(IUserService userService, IBaseService<Notification> notificationService)
+        {
+            _userService = userService;
+            _notificationService = notificationService;
+        }
+        // GET: Notification
+        public ActionResult GetNotification()
+        {
+            UserSession userSession = SessionHelper.GetSession(AppSettingConstant.LoginSessionCustomer) as UserSession;
+            if (userSession!=null)
+            {
+                var user = _userService.Find(u => u.Username == userSession.Username & u.Status == Status.Active & u.Role == UserRole.Customer);
+                if (user!=null)
+                {
+                    var today = DateTime.Now.Date;
+                    var notis = _notificationService.FindAll(n => n.SendTo == user.Username & DbFunctions.TruncateTime(n.CreatedAt)==today).OrderByDescending(n=>n.CreatedAt).Take(4).ToList();
+                    ViewBag.Notifications = notis;
+                }
+            }
+
+            return PartialView("~/Views/Shared/_Notification.cshtml");
+        }
+    }
+}
