74ff8570b24537989b8678a0ef9be22e66fb2fee
Tran Duong <duongtran845@gmail.com>
2018-10-02 18:09:15 +0700

    update

--- Project/WEB/Areas/Admin/Controllers/DashbroadController.cs
+++ Project/WEB/Areas/Admin/Controllers/DashbroadController.cs
@@ -29,24 +29,43 @@ namespace WEB.Areas.Admin.Controllers
             var today = DateTime.Now.Date;
             var privious = DateTime.Now.AddDays(-1).Date;
             var orderPrevious = _orderService.FindAll(o=>o.CreatedAt.Value.Month.Equals(previousMonth) & o.CreatedAt.Value.Year.Equals(previousYear)).ToList();
-            var orderMonth = _orderService.FindAll(o => o.CreatedAt.Value.Month.Equals(month) & o.CreatedAt.Value.Year.Equals(year) & o.Status == OrderStatus.Received).ToList();
-            var orderPending = _orderService.FindAll(o => o.Status == OrderStatus.Pending).ToList();
+            var orderMonth = _orderService.FindAll(o => o.CreatedAt.Value.Month.Equals(month) & o.CreatedAt.Value.Year.Equals(year) & o.Status == OrderStatus.Received).ToList();  
             int imageOfDay = orderMonth.Where(o => o.CreatedAt.Value.Date == today).Sum(od => od.OrderDetails.Sum(o => o.Quantity.Value));
             int imageOfMonth = orderMonth.Sum(od => od.OrderDetails.Sum(o => o.Quantity.Value));
             int imageOfDayPrevious = orderMonth.Where(o => o.CreatedAt.Value.Date == privious).Sum(od => od.OrderDetails.Sum(o => o.Quantity.Value));
             int imageOfMonthPrevious = orderPrevious.Sum(od => od.OrderDetails.Sum(o => o.Quantity.Value));
-
-            float percentImageOfDay = 0;
+            double percentImageOfDay = 0;
+            decimal totalMonth = orderMonth.Sum(o => o.OrderDetails.Sum(od => od.Quantity * (od.Material.Price + od.Product.ProductPrice))).Value;
+            decimal totalPrevious = orderPrevious.Sum(o => o.OrderDetails.Sum(od => od.Quantity * (od.Material.Price + od.Product.ProductPrice))).Value;
+            double percentTotal = 0;
+            double percentOrder = 0;
+            if (orderPrevious.Count>0)
+            {
+                percentOrder = ((orderMonth.Count - orderPrevious.Count) / orderPrevious.Count) * 100;
+            }
+            else
+            {
+                percentOrder = (orderMonth.Count - orderPrevious.Count) * 100;
+            }
+            if (totalPrevious>0)
+            {
+                double percent = Decimal.ToInt32((totalMonth - totalPrevious) / totalPrevious);
+                percentTotal = Math.Round(percent,2) * 100;
+            }
+            else
+            {
+                percentTotal = Decimal.ToInt32((totalMonth - totalPrevious)) * 100;
+            }
             if (imageOfDayPrevious>0)
             {
                 percentImageOfDay = (imageOfDay - imageOfDayPrevious) / imageOfDayPrevious * 100;
             }
             else
             {
-                percentImageOfDay = (imageOfDay - imageOfDayPrevious)* 100;
+                percentImageOfDay = (imageOfDay - imageOfDayPrevious)*100;
             }
             //Percent Day
-            float percentImageOfMonth = 0;
+            double percentImageOfMonth = 0;
             if (imageOfMonthPrevious > 0)
             {
                 percentImageOfMonth = (imageOfMonth - imageOfMonthPrevious) / imageOfMonthPrevious * 100;
@@ -60,22 +79,23 @@ namespace WEB.Areas.Admin.Controllers
             ViewModelDashboard viewModel = new ViewModelDashboard()
             {
                 OrderCount = orderMonth.Count,
-                OrderPending = orderPending.OrderByDescending(o => o.CreatedAt).ToList(),
                 PreviousOrder = orderPrevious.Count,
                 Total = orderMonth.Sum(o => o.OrderDetails.Sum(od => od.Quantity * (od.Product.ProductPrice + od.Material.Price.Value))).Value,
                 TotalPrevious = orderPrevious.Sum(o => o.OrderDetails.Sum(od => od.Quantity * (od.Product.ProductPrice + od.Material.Price.Value))).Value,
                 ImageOfDay = imageOfDay,
                 ImageOfMonth = imageOfMonth,
                 PercentImageOfDay = percentImageOfDay,
-                PercentImageOfMonth = percentImageOfMonth
-
+                PercentImageOfMonth = percentImageOfMonth,
+                PercentTotal = percentTotal,
+                PercentOrder = percentOrder
+                
             };
 
             return View(viewModel);
         }
         public ActionResult GetOrderPending(int num,int page)
         {
-            var orderPending = _orderService.GetAll(num, page, o => o.CreatedAt, o => o.Status == OrderStatus.Pending).ToList();
+            var orderPending = _orderService.GetAll(num, page, o => o.CreatedAt, o => o.Status == OrderStatus.Pending, o => o.CreatedAt).ToList();
             return PartialView("~/Areas/Admin/Views/Dashbroad/_OrderPending.cshtml", orderPending);
         }
     }
--- Project/WEB/Areas/Admin/Models/ViewModelDashboard.cs
+++ Project/WEB/Areas/Admin/Models/ViewModelDashboard.cs
@@ -8,14 +8,15 @@ namespace WEB.Areas.Admin.Models
 {
     public class ViewModelDashboard
     {
-        public List<Order> OrderPending { get; set; }
         public int OrderCount { get; set; }
         public int PreviousOrder { get; set; }
+        public double PercentOrder { get; set; }
+        public double PercentTotal { get; set; }
         public decimal TotalPrevious { get; set; }
         public decimal Total { get; set; }
         public int ImageOfDay  { get; set; }
         public int ImageOfMonth { get; set; }
-        public float PercentImageOfDay { get; set; }
-        public float PercentImageOfMonth { get; set; }
+        public double PercentImageOfDay { get; set; }
+        public double PercentImageOfMonth { get; set; }
     }
 }
