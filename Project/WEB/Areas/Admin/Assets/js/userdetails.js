ec2c0cdfd641f201ca3b9dbf5f5ae5bde78c2867
lovehlmmm <mboxshop92@gmail.com>
2018-10-12 11:25:41 +0700

    update view user info in admin

--- Project/WEB/Areas/Admin/Assets/js/userdetails.js
+++ Project/WEB/Areas/Admin/Assets/js/userdetails.js
@@ -2,4 +2,23 @@
     $('#nav-orders-tab').click(function() {
         GetData();
     });
+
+    $('.showcredit').click(function () {
+        var id = $(this).data('id');
+        $.ajax({
+            url: '/details/GetCreditCard?id=' + id,
+            type: 'GET',
+            success: function (response) {
+                if (response.status) {
+                    $('#showCardNum').val(response.data.CreditNumber);
+                    $('#showCardExpire').val(response.data.Expire);
+                    $('#showCardCVC').val(response.data.CVC);
+                    $('#creditModal').modal();
+                } else {
+                    alert('faill');
+                }
+            }
+        });
+    });
+
 });
